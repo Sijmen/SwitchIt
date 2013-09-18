44604aa6fa1f3f3b2d36ea3d0a8560f01aabafa2
Sijmen <sijmen.hoogendijk@gmail.com>
2013-09-18 13:07:58 +0200

    rename parent folder to prevent problems with erica push. This should solve the problem with {'EXIT',{ucs,{bad_utf8_character_code}}} Removing _id might also be a solution

--- www/views/switches/map.js
+++
@@ -1,7 +0,0 @@
-function(doc){
-	if(doc.doctype.indexOf('switch') > -1)
-		if(doc.type.indexOf('group') == -1)
-			emit([doc.type,doc._id],[doc._id]);
-		else
-			emit([doc.type,doc._id],doc.list) ;
-}
